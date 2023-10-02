017228c830e45824f8553ba79939aee4c2dc515a
Sajjad Mansouri <s.mansouri4w@gmail.com>
2023-10-02 22:47:51 +0330

    add templates

--- polls/views.py
+++ polls/views.py
@@ -1,17 +1,17 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-	return HttpResponse("Hello, world. 5ef19f54 is the polls index.")
-
+	return render(request,'polls/index.html')
 def owner(request):
 	return HttpResponse("Hello, world. 77be07f3 is the polls index.")
 
 def detail(request,question_id):
-	return HttpResponse(f"Hello, world. 77be07f3 is the polls index. question:{question_id}")
+	return render(request,'polls/detail.html',context={'question_id':question_id})
 
 def results(request,question_id):
-	return HttpResponse(f"Hello, world. 77be07f3 is the polls index.qusetion results:{question_id}")
+	return render(request,'polls/results.html',context={'question_id':question_id})
 
 def vote(request,question_id):
 	return HttpResponse(f"Hello, world. 77be07f3 is the polls index.vot  :{question_id}")
