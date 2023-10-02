9834c04cd31d639b81cbc40cd112fcccd996c1e3
Sajjad Mansouri <s.mansouri4w@gmail.com>
2023-10-02 20:30:59 +0330

    httpresponse message change

--- polls/views.py
+++ polls/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. 5ef19f54 is the polls index.")
