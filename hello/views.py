f26b690ae6f8aa099efbdd038eea241f3e2150f8
Sajjad Mansouri <s.mansouri4w@gmail.com>
2023-10-03 19:03:01 +0330

    some change for assignment

--- hello/views.py
+++ hello/views.py
@@ -6,6 +6,6 @@ def myview(request):
 	request.session['num']=num
 	if num>4 :
 		del request.session['num']
-	resp=HttpResponse(f'dcbe0f23,view count:{num}')
+	resp=HttpResponse(f'dcbe0f23,view count={num}')
 	resp.set_cookie('dj4e_cookie', 'dcbe0f23', max_age=1000)
 	return resp
